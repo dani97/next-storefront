64aef14f81c880afa04feb99eca1365145a5827f
Christopher Daniel <cdaniel@ztech.io>
2020-04-18 01:22:08 +0530

    fixed mobile styles

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,20 +4,25 @@ import style from './Footer.module.css';
 
 const Footer = () => (
     <footer className={style.footer}>
-        <div>
-            <Link href="/cms/[uid]" as={"/cms/private-sales"}>
-                <a>Private Sales</a>
-            </Link>
+        <div className={style.cmsLinks}>
+            <div>
+                <Link href="/cms/[uid]" as={"/cms/private-sales"}>
+                    <a>Private Sales</a>
+                </Link>
+            </div>
+            <div>
+                <Link href="/cms/[uid]" as={"/cms/reward-points"}>
+                    <a>Reward points</a>
+                </Link>
+            </div>
+            <div>
+                <Link href="/cms/[uid]" as={"/cms/privacy-policy-cookie-restriction-mode"}>
+                    <a>Private Policy</a>
+                </Link>
+            </div>
         </div>
-        <div>
-            <Link href="/cms/[uid]" as={"/cms/reward-points"}>
-                <a>Reward points</a>
-            </Link>
-        </div>
-        <div>
-            <Link href="/cms/[uid]" as={"/cms/privacy-policy-cookie-restriction-mode"}>
-                <a>Private Policy</a>
-            </Link>
+        <div className={style.about}>
+            PWA built with next JS and Magento 2 - Developed By Chris Daniel
         </div>
     </footer>
 );

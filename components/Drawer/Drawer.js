e85d791ab8638055aac3a769294f7d0f0aba7b33
Christopher Daniel <cdaniel@ztech.io>
2020-05-07 10:51:18 +0530

    error boundary for cart drawer

--- components/Drawer/Drawer.js
+++ components/Drawer/Drawer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './drawer.module.css';
 import cls from 'classnames';
 import dynamic from "next/dynamic";
+import ErrorBoundary from "../Error/ErrorBoundry";
 
 const CategoryTree = dynamic(() => import('./CategoryTree'),{ssr: false});
 
@@ -9,7 +10,10 @@ const Drawer = ({open, title}) => {
     return <>
         <nav className={cls(style.drawer, {[style.open]: open})}>
             <h3 className={style.title}>{title}</h3>
-            <CategoryTree rootId={2}/>
+            <ErrorBoundary>
+                <CategoryTree rootId={2}/>
+            </ErrorBoundary>
+
         </nav>
     </>
 }
